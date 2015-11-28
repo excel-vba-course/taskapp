502d10588bc6f1c9254133be33066a8011129790
priyank gupta <priyank1985gupta@rediffmail.com>
2015-11-28 18:48:04 +0100

    refactored filter

---
+++ www/js/filters/inBetween.js
@@ -0,0 +1,13 @@
+var taskModule = angular.module('kanban.controllers');
+taskModule.filter("inbetween", function() {
+  return function(items, from, to, property) {
+        var arrayToReturn = [];        
+        for (var i=0; i<items.length; i++){
+            var fieldValue = parseInt(items[i][property]);
+        if (fieldValue > from && fieldValue < to)  {
+                arrayToReturn.push(items[i]);
+            }
+        }
+        return arrayToReturn;
+  };
+});
