24e888e79c378bb106d44b192dacdc9d0d726289
priyank gupta <priyank1985gupta@rediffmail.com>
2015-12-01 11:13:55 +0100

    30 incorrect credentials not handled

--- www/js/controllers/kanban.sign-in.controller.js
+++ www/js/controllers/kanban.sign-in.controller.js
@@ -2,6 +2,7 @@ var taskModule = angular.module('kanban.controllers');
 
 var KanboardSignInController = function($ionicLoading, $scope,  $ionicActionSheet, $q,  $http, $window, $base64, $ionicPopup) {
 	var createConfig = function() {
+		console.log($window.localStorage["session"]);
 		var session = JSON.parse($window.localStorage["session"]);
 		var auth = $base64.encode(session.username + ":" + session.password);
 		var config = {
@@ -18,24 +19,31 @@ var KanboardSignInController = function($ionicLoading, $scope,  $ionicActionShee
 	}
 
 	$scope.signIn = function(session) {
-		$ionicLoading.show({
-			template: 'Loading...'
-		});
-		var request = '{"jsonrpc": "2.0","method": "getMe", "id": 133280317}';
-		$http.post(api_endpoint, request, createConfig()).success(function(request) {
-			$window.localStorage["me"] = JSON.stringify(request.result);
-			$scope.me = JSON.parse($window.localStorage["me"]);
+		if(session){
+			$ionicLoading.show({
+				template: 'Loading...'
+			});
 			$window.localStorage["session"] = JSON.stringify(session);
-			$ionicLoading.hide();
-		}).error(function(err){
-			console.log(err);
-			$ionicLoading.hide();
+			var request = '{"jsonrpc": "2.0","method": "getMe", "id": 133280317}';
+			$http.post(api_endpoint, request, createConfig()).success(function(request) {
+				$window.localStorage["me"] = JSON.stringify(request.result);
+				$scope.me = JSON.parse($window.localStorage["me"]);
+				$ionicLoading.hide();
+			}).error(function(err){
+				console.log(err);
+				$ionicLoading.hide();
+				$ionicPopup.alert({
+					title: 'Login Error',
+					template: err.error
+				});
+
+			});
+		}else{
 			$ionicPopup.alert({
 				title: 'Login Error',
-				template: err.error
+				template: "Please provide login information"
 			});
-
-		});
+		}
 	}
 	if($window.localStorage["me"]) {
 		$scope.me = JSON.parse($window.localStorage["me"]);
