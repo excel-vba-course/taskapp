ab3cc8c6d7fcf8958fd1712b4812d2e04a6a13f4
priyank gupta <priyank1985gupta@rediffmail.com>
2015-12-01 09:20:08 +0100

    showing pop up for login error

--- www/js/controllers/kanban.sign-in.controller.js
+++ www/js/controllers/kanban.sign-in.controller.js
@@ -1,15 +1,15 @@
 var taskModule = angular.module('kanban.controllers');
 
-var KanboardSignInController = function($ionicLoading, $scope,  $ionicActionSheet, $q,  $http, $window, $base64) {
+var KanboardSignInController = function($ionicLoading, $scope,  $ionicActionSheet, $q,  $http, $window, $base64, $ionicPopup) {
 	var createConfig = function() {
 		var session = JSON.parse($window.localStorage["session"]);
-    var auth = $base64.encode(session.username + ":" + session.password);
-    var config = {
-      headers: {
-        'Authorization': 'Basic ' + auth
-      }
-    };
-    return config;
+		var auth = $base64.encode(session.username + ":" + session.password);
+		var config = {
+			headers: {
+				'Authorization': 'Basic ' + auth
+			}
+		};
+		return config;
 	};
 
 	$scope.logout = function() {
@@ -19,14 +19,22 @@ var KanboardSignInController = function($ionicLoading, $scope,  $ionicActionShee
 
 	$scope.signIn = function(session) {
 		$ionicLoading.show({
-	    template: 'Loading...'
-	  });
-		$window.localStorage["session"] = JSON.stringify(session);
+			template: 'Loading...'
+		});
 		var request = '{"jsonrpc": "2.0","method": "getMe", "id": 133280317}';
 		$http.post(api_endpoint, request, createConfig()).success(function(request) {
 			$window.localStorage["me"] = JSON.stringify(request.result);
 			$scope.me = JSON.parse($window.localStorage["me"]);
+			$window.localStorage["session"] = JSON.stringify(session);
 			$ionicLoading.hide();
+		}).error(function(err){
+			console.log(err);
+			$ionicLoading.hide();
+			$ionicPopup.alert({
+				title: 'Login Error',
+				template: err.error
+			});
+
 		});
 	}
 	if($window.localStorage["me"]) {
@@ -34,5 +42,6 @@ var KanboardSignInController = function($ionicLoading, $scope,  $ionicActionShee
 	}
 }
 
-taskModule.controller('KanboardSignInController', ['$ionicLoading', '$scope',  '$ionicActionSheet', '$q',  '$http', '$window', '$base64', KanboardSignInController]);
+taskModule.controller('KanboardSignInController', ['$ionicLoading', '$scope',
+	'$ionicActionSheet', '$q',  '$http', '$window', '$base64', '$ionicPopup', KanboardSignInController]);
 
